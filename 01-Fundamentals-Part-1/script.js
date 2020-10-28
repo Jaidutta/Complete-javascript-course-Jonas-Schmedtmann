16a39e1f5a372d26b3684d2dc3c526ad2e90909e
Joy Dutta <dutta_joy@hotmail.com>
2020-10-28 10:51:10 +0000

    Adds 01-Fundamentals-Part-1 and Javascript Fundamentals-Part-1-Assignments

---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,89 @@
+let js = "amazing"
+if (js === "amazing") alert('Javascript is FUN!')
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23)
+
+let firstName = 'Jonas'
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+// variable naming conventions
+let jonas_matilda = "JM"
+let $function = 27;
+
+let person = "jonas"
+let PI = 3.1415;
+
+let myFirstJob = 'Programmer'
+let myCurrentJob = 'Teacher'
+
+let job1 = 'programmer'
+let job2 = 'teacher'
+
+let javascriptIsFun = true
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptisFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES'
+console.log(typeof java);
+
+let year
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// JS declaration
+let age = 30;
+age = 31
+
+const birthYear = 1990
+// birthYear = 1991
+
+//const job 
+
+var job = 'Programmer'
+job = 'Teacher'
+
+lastName = 'Schmedtmann'
+
+// Math Operators
+const now = 2037
+const ageJonas = now -1991;
+console.log(ageJonas);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 * 2 * 2 = 8
+
+const ageSarah = now - 2018
+console.log(ageJonas, ageSarah);
+
+const myFirstName = 'Joy'
+const myLastName = 'Dutta'
+
+console.log(myFirstName +  ' ' + myLastName);
+
+// Assignment Operators
+let x = 10 + 5; // 15
+x += 10 // x = x + 10 => 15 + 10
+x *= 4
+x++
+x--
+console.log(x);
+
+// Comparison Operators: The result of comparison operators should be boolean
+// >, <, >=, <=
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+
+console.log(now - 1990 > now - 2020);
